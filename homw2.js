28f48978ea11bcee8e83adb8b417e83e4ac908fb
Anano121 <achezhia08@gmail.com>
2024-03-20 23:28:47 +0400

    2nd

---
+++ homw2.js
@@ -0,0 +1,56 @@
+let a=87;
+let b=65;
+console.log(a+b);
+
+let student={
+    firstname:"anano",
+    lastname:"chezhia",
+    age:20,
+    email:"achezhia08@gmail.com",
+    marks:10,
+
+    
+};
+console.log(student.firstname)
+let m=45
+let n=23
+
+if (m>n) {
+    console.log(m+n)
+}
+else {
+    console.log(n)
+};
+
+let studentname="anano"
+
+
+console.log ("rise and shine "+ studentname  )
+
+
+let bonus=1;
+let salary=2;
+if (salary >=2000){
+    console.log (bonus=0)
+}
+else if (salary<2000) 
+{
+console.log(bonus=salary*0.1)
+};
+
+ a=23, b=a/2, c=a+b 
+ console.log(c)
+
+ let phones= [ "Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi" ] 
+ console.log(phones);
+
+ phones.push("motorola")
+ console.log(phones);
+
+ let i=1;
+  
+
+ for (i;i<=12; i++ ) {
+    console.log(i)
+ };
+
